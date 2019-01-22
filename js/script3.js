a9ddc1b0b8ab3063990bcdbf7c8294aa9e371147
Ali-X <alina240895@yandex.ru>
2019-01-22 21:24:29 +0200

    Implement third task

---
+++ js/script3.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function createSpanInBlockByID(blockID) {
+  let elementById = document.getElementById(blockID);
+  if (elementById != null) {
+    let spanElement = elementById.querySelector("span");
+
+    if (spanElement == null) {
+      spanElement = document.createElement("span");
+      spanElement.textContent = "new span";
+      elementById.appendChild(spanElement);
+    }
+  } else {
+    console.log("No tag with id " + blockID);
+  }
+}
+
+createSpanInBlockByID("testBlock");
