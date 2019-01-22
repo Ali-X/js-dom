35324c1115f41b18d80473f1fd896d8af782977a
Ali-X <alina240895@yandex.ru>
2019-01-22 20:21:44 +0200

    Implement second task

---
+++ js/script2.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function getCellDiagonal() {
+  let rows = document.getElementsByTagName("tr");
+
+  for (let i = 0; i < rows.length; i++) {
+    rows[i].getElementsByTagName("td")[i].style.background = 'red';
+  }
+}
+
+getCellDiagonal();
