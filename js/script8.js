1df6b60c20a4179f977faae062cec691fa435516
Ali-X <alina240895@yandex.ru>
2019-01-24 21:21:23 +0200

    Add skeleton for eighth task

---
+++ js/script8.js
@@ -0,0 +1,14 @@
+"use strict";
+
+for (let i = 0; i < ANCESTRY_FILE.length; i++) {
+  
+}
+
+function getAverageAgeBtwMomAndChildren() {
+  let summOfAverages;
+
+  for (let i = 0; i < ANCESTRY_FILE.length; i++) {
+
+  }
+
+}
